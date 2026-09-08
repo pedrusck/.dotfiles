a0dbbe6ac33218df2d6a1aa813e89af2dc7a8c1e
Pere Bohigas <pere.bohigas@gmail.com>
2026-09-08 19:13:05 +0200

    Add mise configuration file for specific company configuration

--- mise/setup.sh
+++ mise/setup.sh
@@ -7,3 +7,7 @@ TOOL_DIR="${DOTFILES_PATH:=$PWD}/mise"
 mkdir -p "$HOME/.config/mise"
 ln -sf "$TOOL_DIR/config.toml" "$HOME/.config/mise/config.toml"
 command -v mise &>/dev/null && mise trust "$HOME/.config/mise/config.toml"
+
+mkdir -p "$HOME/Developer/adesso_projects"
+ln -sf "$TOOL_DIR/mise_company_configuration.secret.toml" "$HOME/Developer/adesso_projects/mise.toml"
+command -v mise &>/dev/null && mise trust "$HOME/Developer/adesso_projects/mise.toml"
